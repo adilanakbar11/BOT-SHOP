58eb54d7f7c0ee7ed68be016d8569b4d460c6d3b
adilanakbar11 <adilanakbar11@gmail.com>
2021-10-07 09:13:12 +0700

    BOT-SHOP

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `💸 PAYMENT LAXY STORE 💸
+
+*さ BANK*
+1. BRI 
+	ㅁ GAK ADA
+		• R***A*DI
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 082117489822
+		• ADILAN AKBAR
+2. OVO
+	ㅁ 082117489822
+		• TAN***AH
+3. DANA
+	ㅁ 082117489822
+		• TAN***AH
+
+*さ PULSA*
+1. SMARTFREN
+	ㅁ GAK ADA
+2. TELKOMSEL
+	ㅁ GAK ADA
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
